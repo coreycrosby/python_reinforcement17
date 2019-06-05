fac255e45ebeeb5a0d59541d66cae0a74acf9da0
Corey Crosby <ccc_13@live.ca>
2019-06-05 15:24:11 -0400

    completed exercise

---
+++ exercise.py
@@ -0,0 +1,20 @@
+dict = {}
+
+
+def function():
+    for n in range(1, 51):
+        if n % 2 == 0 and n % 7 == 0:
+            dict[n] = n * 2
+        elif n % 2 == 0:
+            dict[n] = n + 1
+        else:
+            dict[n] = n
+    return dict
+
+
+print(function())
+
+
+
+
+
